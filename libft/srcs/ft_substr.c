448b71f9ba86d6e4bd5c1f7c1ef141f5a9d72f74
pohl <pohl@student.42.fr>
2021-01-10 16:52:45 +0100

    ft_substr created leaks, fixed it.

--- libft/srcs/ft_substr.c
+++ libft/srcs/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: pohl <pohl@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 14:02:15 by pohl              #+#    #+#             */
-/*   Updated: 2019/10/17 18:28:04 by pohl             ###   ########.fr       */
+/*   Updated: 2021/01/10 16:32:07 by pohl             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,43 @@
 #include <stdlib.h>
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *string, unsigned int start, size_t len)
 {
-	size_t			i;
-	char			*buffer;
-	unsigned int	s_len;
+	size_t	i;
+	size_t	string_len;
+	char	*result;
 
-	i = 0;
-	if (s == NULL)
+	if (!string)
 		return (NULL);
-	s_len = ft_strlen(s);
-	while (s[start + i])
-		i++;
-	if (s_len > start && ((buffer = (char *)malloc(1)) == NULL))
+	string_len = ft_strlen(string);
+	if (start >= string_len)
+	{
+		if (!(result = malloc(sizeof(string))))
+			return (NULL);
+		result[0] = 0;
+		return (result);
+	}
+	if (!(result = malloc(sizeof(string) * ft_strlen(string + start) + 1)))
 		return (NULL);
-	else if (i >= len && ((buffer = (char *)malloc(len + 1)) == NULL))
-		return (NULL);
-	else if ((buffer = (char *)malloc(i + 1)) == NULL)
-		return (NULL);
-	i = 0;
-	if (s_len > start)
-		while (i < len && s[start + i])
-		{
-			buffer[i] = s[start + i];
-			i++;
-		}
-	buffer[i] = 0;
-	return (buffer);
+	i = -1;
+	while (++i < len && string[start + i])
+		result[i] = string[start + i];
+	result[i] = 0;
+	return (result);
 }
+
+// #include <stdio.h>
+
+// int		start(void)
+// {
+// 	char *testest = ft_substr("char const *s", 2, 112);
+// 	printf("%p, %s\n", testest, testest);
+// 	free(testest);
+// 	return (0);
+// }
+
+// int		main(void)
+// {
+// 	start();
+// 	system("leaks useless");
+// }
