3c4c6f79a880afae76e0c2f540027bbd72e318d0
popohl <pohl@student.42.fr>
2021-02-07 20:19:46 +0100

    normed singleton.c

--- srcs/singleton.c
+++ srcs/singleton.c
@@ -6,20 +6,21 @@
 /*   By: elbouju <elbouju@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/29 10:38:12 by elbouju           #+#    #+#             */
-/*   Updated: 2021/01/29 10:40:00 by elbouju          ###   ########.fr       */
+/*   Updated: 2021/02/07 20:19:37 by paulohl          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "libft.h"
 
-t_command   *singleton(void)
+t_command	*singleton(void)
 {
-	static t_command *cmd = NULL;
+	static t_command	*cmd = NULL;
 
 	if (cmd)
 		return (cmd);
-	if (!(cmd = ft_calloc(sizeof(t_command), 1)))
+	cmd = ft_calloc(sizeof(t_command), 1);
+	if (!cmd)
 		return (NULL);
 	return (cmd);
 }
